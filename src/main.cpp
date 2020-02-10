8d5208c35e92c20d18405c2ec650c5128298593b
sirsimon771 <simon.dischl@yahoo.de>
2020-02-10 16:15:00 +0100

    added some comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 
 int i = 0;
 
+/*Matrix wiring layout:
+
+
+
+*/
+
 void setup() {
 
   Serial.begin(9600);
@@ -16,3 +22,15 @@ void loop() {
   Serial.print(i);
   i++;
 }
+
+/*  Joystick Library functions
+
+Joystick.begin(bool initAutoSendState)
+Joystick.end()
+Joystick.sendState()
+
+Joystick.pressButton(uint8_t button)
+Joystick.releaseButton(uint8_t button)
+Joystick.setButton(uint8_t button, uint8_t value)
+
+*/
