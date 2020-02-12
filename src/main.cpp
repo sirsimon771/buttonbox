24154b2661b5146217f3a0edd73729cac4d76e20
sirsimon771 <simon.dischl@yahoo.de>
2020-02-12 15:58:08 +0100

    changed matrix & wiring layout to use interrupt pins for rotary encoders

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,17 @@ Joystick.setButton(uint8_t button, uint8_t value)
 
 /*Matrix wiring layout:
 
-    6   7   8   9   10
-0   B1  B2  B3  B4  T1
-1   B5  B6  B7  B8  T2
-2   B9  B10 B11 B12 T3
-3   T4  T5  R1  R2  R3
-4   R4  R5  S1a  S1b S2a
-5   S2b
+    14 |15 |16 |18 |19
+4   B1 |B2 |B3 |B4 |T1
+5   B5 |B6 |B7 |B8 |T2
+6   B9 |B10|B11|B12|T3
+8   T4 |T5 |R1 |R2 |R3
+9   R4 |R5 |S1a|S1b|S2a
+10  S2b|
 
-14      15      16      18      19      20
-R1p1    R2p1    R3p1    R4p1    R5p1    R*p2
+interrupt pins for rotary encoders phase 1
+3(INT0)|2(INT1)|0(INT2)|1(INT3)|7(INT6)|20
+R1p1   |R2p1   |R3p1   |R4p1   |R5p1   |R*p2
 
 */
 
