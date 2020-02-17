4c8a723404aa17388d331aa84ba9b45e4c84ebee
sirsimon771 <simon.dischl@yahoo.de>
2020-02-17 18:05:21 +0100

    implemented rotary encoders copied earlier draft, much room for improvement & optimization

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,13 @@
 //creating the joystick object
 Joystick_ Joystick;
 
+//create rotary encoder objects
+RotaryEncoder re[5];
+
+//phase 2 pin number
+const int pinPhase2 = 20;
+const int pins[5] = {3, 2, 0, 1, 7};
+
 //number of buttons used
 const int buttons = 25;
 
@@ -44,6 +51,67 @@ void setPinModes();
 void readMatrixNum();
 void initializeToggles();
 
+/////rotary encoder class/////
+class RotaryEncoder
+{
+private:
+  int button1Num, button2Num; //button1 = cw || button2 = ccw
+  int pinPhase1, pinPhase2;
+  volatile bool lastcw, lastccw = false;
+
+public:
+  void start();       //set pinMode
+  void turn();        //set joystick buttons
+  void release();     //release joystick buttons (implement delay/holdtime of 20ms)
+  int pinNum();       //output the pin number
+  int pinInt();       //output the interrupt number
+  void setPin(int p); //set pin number
+  void setB1(int b1); //set button 1 number
+  void setB2(int b2); //set button 2 number
+
+  //constructor, takes pin phase 1 number & 2 button numbers
+  RotaryEncoder(int pin, int b1, int b2)
+  {
+    pinPhase1 = pin;
+    button1Num = b1;
+    button2Num = b2;
+  }
+
+  //constructor overload for array of objects
+  RotaryEncoder()
+  {
+    pinPhase1 = -1;
+    button1Num = -1;
+    button2Num = -1;
+  }
+};
+
+/////rotary encoder methods/////
+
+void RotaryEncoder::start() //set pinMode
+{
+  pinMode(pinPhase1, INPUT);
+}
+
+void RotaryEncoder::turn()
+// code to check for turning direction
+// and pushing the joystick buttons
+{
+}
+
+void RotaryEncoder::release()
+//code to release both buttons of phase1 is low and one of them is pressed
+{
+}
+
+int RotaryEncoder::pinNum() { return pinPhase1; }
+
+int RotaryEncoder::pinInt() { return digitalPinToInterrupt(pinPhase1); }
+
+void RotaryEncoder::setPin(int p) { pinPhase1 = p; }
+void RotaryEncoder::setB1(int b1) { button1Num = b1; }
+void RotaryEncoder::setB1(int b2) { button2Num = b2; }
+
 /*Matrix wiring layout:
 
     14 |15 |16 |18 |19
@@ -74,6 +142,22 @@ void setup()
   setPinModes();        //sets all pins to the right mode
   readMatrixNum();      //fill num[] array
   initializeToggles();  //set all toggles to initial states (press no buttons)
+
+  //initialize rotary encoder object in array with values
+      for (int i = 0; i < 5; i++)
+    {
+        int b = 0;
+        re[i].setPin(pins[i]);
+        re[i].setB1(b);
+        b++;
+        re[i].setB2(b);
+        b++;
+    }
+    //attach interrupts
+    for (int i = 0; i < 5; i++)
+    {
+        attachInterrupt(re[i].pinInt, re[i].turn, RISING);
+    }
 }
 
 void loop()
@@ -146,7 +230,21 @@ void loop()
       }
     }
   }
+
+  //release joystick buttons for rotary encoders
+  for (int i = 0; i < 5; i++)
+  {
+    re[i].release();
+  }
 }
+
+
+
+
+
+
+
+
 
 /////used functions/////
 void setPinModes()  //sets all pins to the right mode
